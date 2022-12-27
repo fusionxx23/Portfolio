b0a1a6b9b5bfe07c01067a0416b80b5094dbacb9
TarpCityBitch <travistarpley0@gmail.com>
2022-12-26 16:52:36 -0800

    added techs to about

--- components/About.tsx
+++ components/About.tsx
@@ -2,30 +2,57 @@ import React, { useEffect, useState } from 'react'
 import Image from 'next/image';
 import doodle from '../public/images/sun_pfp.png'
 import { InView, useInView } from 'react-intersection-observer';
+import { SiNextdotjs, SiSolidity, SiTypescript, SiDocker, SiTailwindcss } from 'react-icons/si';
 let text = 'Fast-forward to today, and I’ve had the privilege of working at an advertising agency, a start-up, a huge corporation, and a student-led design studio. My main focus these days is building accessible, inclusive products and digital experiences at Upstatement for a variety of clients.'
 
 
 export default function About() {
-  const [display, setDisplay] = useState(false); 
+  const [display, setDisplay] = useState(false);
   const anim = 'transition-all ease-out '
   return (
     <InView onChange={(inView, _entry) => {
-      if(inView){
-        setDisplay(true); 
-    
+      if (inView) {
+        setDisplay(true);
+
       }
     }} style={{ minHeight: '200px' }} className={`duration-700 h-screen px-10 transition-all ${anim} ${display ? ' opacity-100 translate-x-0 translate-y-0 ' : 'opacity-0 translate-y-24 translate-x-24'}`}>
-      <h1 className='text-light-200 font-chivo font-bold z-40 mb-10 text-2xl sm:text-4xl' >About Me<span className='text-light-100'>.</span></h1>
+      <h1 className='text-light-200 font-chivo font-bold z-40 mb-8 text-2xl sm:text-4xl' >About Me<span className='text-light-100'>.</span></h1>
       <div className='flex flex-wrap md:flex-nowrap justify-between'>
-        <div className='text-light-200 text-sm font-chivo'>
+        <div className='text-light-200 text-md font-chivo'>
           <h2>{text}</h2>
+          <h2 className='mt-8 text-light-200 font-bold text-lg font-poppins'>Technologies I{`'`}ve worked with recently<span className='text-light-100'>:</span></h2>
+          <div className='flex flex-wrap mt-2 px-2'>
+            <div className='flex items-center mr-4'>
+              <div className='w-10 bg-light-100' style={{ height: '2px' }}>
+              </div>
+            </div>
+
+            <TechIcon Icon={SiNextdotjs} title={'Next.js'}></TechIcon>
+            <TechIcon Icon={SiSolidity} title={'Solidity'}></TechIcon>
+            <TechIcon Icon={SiTypescript} title={'Typescript'}></TechIcon>
+            <TechIcon Icon={SiDocker} title={'Docker'}></TechIcon>
+            <TechIcon Icon={SiTailwindcss} title={'Tailwindcss'}></TechIcon>
+            <div className='flex items-center mr-4'>
+              <div className='w-10 bg-light-100' style={{ height: '2px' }}>
+              </div>
+            </div>
+          </div>
         </div>
+
         <ImageSection />
       </div>
     </InView>
   )
 }
 
+function TechIcon({ Icon, title }: { Icon: any, title: string }) {
+  return (
+    <div className='mr-4 my-2 flex flex-col items-center'>
+      <h1 className='mb-1'>{title}</h1>
+      <Icon className='text-light-100' size={20} />
+    </div>
+  )
+}
 
 function ImageSection() {
   const [hovering, setHovering] = useState(false);
--- components/SocialsLeft.tsx
+++ components/SocialsLeft.tsx
@@ -1,33 +1,37 @@
-import { FiGithub, FiTwitter, FiMail, FiLinkedin} from 'react-icons/fi'
-import {RxDiscordLogo} from 'react-icons/rx'
-import {display} from '../libs/types'; 
+import { FiGithub, FiTwitter, FiMail, FiLinkedin } from 'react-icons/fi'
+import { RxDiscordLogo } from 'react-icons/rx'
+import { display } from '../libs/types';
 
 
 
-export default function SocialsLeft({display} : display) {
+export default function SocialsLeft({ display }: display) {
     return (
-        <div className={" hidden md:block md:fixed bottom-4 md:left-2 lg:left-14 font-chivo "}>
+        <div className={" hidden md:block md:fixed bottom-0 md:left-2 lg:left-14 font-chivo "}>
             <div className={` flex-col  justify-center items-center ease-out transition-all duration-500 delay-1200 ${display ? 'translate-x-0 opacity-100' : '-translate-x-4 opacity-0'}`}>
-                <div className='flex justify-center mb-1'>
-                    <div className='h-28 bg-white ' style={{ width: '1px' }}>
+                <div className='flex justify-center mb-4'>
+                    <div className='h-20 bg-white ' style={{ width: '1px' }}>
                     </div>
                 </div>
 
                 <div className={`font-chivo text-light-200 `}>
-                    <SocialIcon Icon={FiGithub}/>  
-                    <SocialIcon Icon={RxDiscordLogo}/>  
-                    <SocialIcon Icon={FiTwitter}/>
-                    <SocialIcon Icon={FiMail}/>
-                    <SocialIcon Icon={FiLinkedin}/>
+                    <SocialIcon Icon={FiGithub} />
+                    <SocialIcon Icon={RxDiscordLogo} />
+                    <SocialIcon Icon={FiTwitter} />
+                    <SocialIcon Icon={FiMail} />
+                    <SocialIcon Icon={FiLinkedin} />
                 </div>
 
+                <div className='flex justify-center mt-4'>
+                    <div className='h-20 bg-white ' style={{ width: '1px' }}>
+                    </div>
+                </div>
             </div>
 
         </div>
     )
 }
 
-function SocialIcon({Icon}: {Icon:any}) {
+function SocialIcon({ Icon }: { Icon: any }) {
     return (
         <div className='px-10 hover:scale-125 py-4 cursor-pointer hover:-translate-y-1 hover:text-light-100 transition-all ease-out'>
             <Icon className='' size={20} />
