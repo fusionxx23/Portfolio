9d435d566703cefdcb7c276e391ab817f1f76a8d
TarpCityBitch <travistarpley0@gmail.com>
2022-12-27 18:24:00 -0800

    scroll now

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,7 +32,7 @@ export default function Home() {
       <div className='bg-dark-300 w-full '>
 
         <Navbar display={loaded}></Navbar>
-        <div className='w-full px-7 sm:px-28 md:px-20 lg:px-48 xl:px-80 2xl:px-108 overflow-y-hidden overflow-x-hidden ' id='mainPage'>
+        <div className='w-full px-7 sm:px-28 md:px-20 lg:px-48 xl:px-80 2xl:px-108 h-screen overflow-x-hidden ' id='mainPage'>
 
           <Landing display={loaded} />
           <About></About>
