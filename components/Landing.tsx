bfe68aa5ebf8b9931ac5f16c8da5f7bb2a5fa142
TarpCityBitch <travistarpley0@gmail.com>
2022-12-27 16:03:51 -0800

    apple animation fix?

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -12,7 +12,7 @@ export default function Landing({ display }: { display: boolean }) {
                 <h3 className={`text-3xl md:text-6xl delay-1000  ${dis} ${anim}`} style={{ color: '#8892b0' }}>Im a Full-Stack Dapp Developer</h3>
                 <h4 className={`text-sm md:text-md md:w-104 mt-7  font-chivo text-light-400 delay-1000 ${dis} ${anim}`}>I’m a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building accessible, human-centered products at Upstatement.</h4>
                 <div className={`flex mt-14 delay-1000 font-chivo ${dis} ${anim}`} >
-                    <div className='px-6 py-3 border border-light-100 rounded-md cursor-pointer transition-all ease-out duration-500 hover:bg-light-100 hover:bg-opacity-30'>
+                    <div className='px-6 py-3 border border-light-100 rounded-md cursor-pointer transition-up ease-out duration-500 hover:bg-light-100 hover:bg-opacity-30'>
                         <h1 className='text-md'>Check out my work!</h1>
                     </div>
                 </div>
