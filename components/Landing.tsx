2ac3caf5e8e446b57d6f1b29e86461227fb59edb
TarpCityBitch <travistarpley0@gmail.com>
2023-01-05 18:46:00 -0800

    Landing Comment

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -11,7 +11,7 @@ export default function Landing({ display }: { display: boolean }) {
                 <h2 className={`mb-5 delay-500 text-md md:text-lg pl-1 ${dis} ${anim} font-chivo`}>Hi, my name is</h2>
                 <h1 className={`text-4xl md:text-7xl delay-700 mb-3 font-bold ${dis} ${anim}`} style={{ color: '#ccd6f6' }}>Travis Tarpley</h1>
                 <h3 className={`text-3xl md:text-6xl delay-1000 ${dis} ${anim}`} style={{ color: '#8892b0' }}>Im a Full-Stack Dapp Developer</h3>
-                <h4 className={`text-sm md:text-md md:w-104 mt-7  font-chivo text-light-400 delay-1000 ${dis} ${anim}`}>I’m a software engineer specializing in building exceptional digital experiences. Currently, I’m focused on building both front and backend decentralized applications.</h4>
+                <h4 className={`text-sm md:text-md md:w-104 mt-7  font-chivo text-light-400 delay-1000 ${dis} ${anim}`}>I’m a software engineer specializing in building exceptional digital experiences. Currently, I’m focused on building applications for the decentralized web.  </h4>
                 <div className={`flex mt-14 delay-1000 font-chivo ${dis} ${anim}`} >
                     <Link href={'#Work'}>
                         <div className='px-6 py-3 border border-light-100 transition-all rounded-md cursor-pointer ease-out  hover:bg-light-100 hover:bg-opacity-30'>
