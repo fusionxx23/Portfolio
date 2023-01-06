7cc8c2354e45ef4c026f1d34f4897986d627c529
TarpCityBitch <travistarpley0@gmail.com>
2023-01-05 18:33:50 -0800

    profile man circle fix for mobile

--- components/About/ProfileImage.tsx
+++ components/About/ProfileImage.tsx
@@ -8,7 +8,7 @@ export default function ImageSection() {
       <div  className={' w-full flex justify-start sm:ml-10 mt-5 sm:w-64 sm:h-64 md:mt-0 md:w-80 md:h-96'}>
         <div  className='z-20 w-48 md:w-64 lg:w-72 2xl:w-80 relative'>
           <Image onMouseOver={() => { console.log('hovering?'); setHovering(true) }} onMouseOut={() => { setHovering(false) }}  className='rounded-full relative z-10 ' src={sun} alt={''}></Image>
-          <div  className={`absolute z-0 cursor-pointer   ${hovering ? 'top-2 left-2' : 'sm:top-5 sm:left-5'} transition-all border-light-100 border-2 w-48 h-48 md:h-64 md:w-64 lg:h-72 lg:w-72 2xl:w-80 2xl:h-80 rounded-full`}></div>
+          <div  className={`absolute z-0 cursor-pointer   ${hovering ? 'top-3 left-3' : 'sm:top-5 sm:left-5 top-2 left-2'} transition-all border-light-100 border-2 w-48 h-48 md:h-64 md:w-64 lg:h-72 lg:w-72 2xl:w-80 2xl:h-80 rounded-full`}></div>
         </div>
       </div>
     )
